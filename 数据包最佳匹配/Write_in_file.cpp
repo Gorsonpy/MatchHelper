777c552fc108b4a062c4fd09df2204da0267152a
Gorsonpy <Gorsonpy@github.com>
2022-01-27 00:42:32 +0800

    add res.txt

--- 数据包最佳匹配/Write_in_file.cpp
+++ 数据包最佳匹配/Write_in_file.cpp
@@ -7,11 +7,12 @@
 
 using std::ofstream;
 using std::endl;
+using std::ios;
 
 void Result_In_File(string &file_name, vector<Data> &datalist,
   vector<Rule> &rulelist)
 {
-  ofstream fout(file_name);
+  ofstream fout(file_name, ios::app);
   auto ans = DoMatch(datalist, rulelist);
   for (auto i : ans)
   {
